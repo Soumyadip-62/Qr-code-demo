0d86a4e96edd918fc2de78d7b93c4ccc21c56628
Soumyadip-62 <panditsoumya35@gmail.com>
2022-11-03 18:26:12 +0530

    some design changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Scanner from './components/Scanner';
 import LOGO from './assets/Kolkata_Thunderbolts_official_logo.png'
+// import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 // import './App.css'
 
-
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
 
 function App() {
   const [count, setCount] = useState(0)
-  const [latitute, setlatitute] = useState(null)
-  const [longtitude, setlongtitude] = useState(null)
-  const [qr_result, setqr_result] = useState('')
+  const [latitute, setlatitute] = useState(null);
+  const [longtitude, setlongtitude] = useState(null);
+  const [qr_result, setqr_result] = useState(null)
 
   function getLocation (){
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -34,40 +38,64 @@ function App() {
 
   return (
     <div>
-      <div className="row justify-content-md-center text-center">
+      <div className=" text-center">
         <img
-          style={{ width: "130px", height: "120px" }}
-          className="col-2"
+          style={{ width: "100px", height: "100px" }}
+          className=""
           src={LOGO}
         />
-        <h1 className="col-4">Ball Tracking System</h1>
+        <h2 className="main-heading">Ball Tracking System</h2>
       </div>
 
       <Scanner
+      
         fps={10}
-        qrbox={250}
+        qrbox={200}
         disableFlip={false}
         qrCodeSuccessCallback={onNewScanResult}
       />
-      <div className="row justify-content-md-center">
-        <div className="col text-center">
-          {qr_result && (
-            <h2>
-              Scanned Result :<span className="scanned"> {qr_result}</span>
+      <div className="row justify-content-md-center p-4">
+        {qr_result && <div className="col p-4 m-2 location-wrapper">
+          
+            <h2 className="small-header">
+              Scanned Result <span className="scanned"> {qr_result}</span>
             </h2>
-          )}
         </div>
+          }
+          
 
         {longtitude && (
-          <div className="col text-center ">
-            <h2>Your Current Location </h2>
-            <h3 className="location">Latitude : {latitute}</h3>
-            <h3 className="location">Longtitude : {longtitude}</h3>
+          <div className="col p-4 m-2 location-wrapper">
+            <h2 className="small-header">Your Current Location</h2>
+            <div className="">
+              <h3 className="location">Latitude : {latitute}</h3>
+              <h3 className="location">Longtitude : {longtitude}</h3>
+            </div>
           </div>
         )}
       </div>
+
+      {/* <div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+            lat: latitute,
+            lng: longtitude,
+          }}
+        >
+          <Marker
+            position={{ lat: latitute, lng: longtitude }}
+            name={"You Are Here"}
+          />
+        </Map>
+      </div> */}
     </div>
   );
 }
 
 export default App
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCJ93bv8-XgalSZ8Pu28r472Lq0ZwCHtuY",
+// })(App);
